562e137007e216a991ec28387951c49a22a92c6d
ToxOptimism <toxicoptimismben@gmail.com>
2025-01-16 22:01:03 +0800

    Added exception handling, Added exception throwing for events, Updated test cases

--- src/main/java/Aurora.java
+++ src/main/java/Aurora.java
@@ -17,6 +17,10 @@ public class Aurora {
         System.out.println("=======================");
     }
 
+    public static void printException(Exception e) {
+        printMsg(e.getMessage());
+    }
+
     public static void addToList(Task t) {
         taskList.add(t);
         printMsg(ADD_TASK + "\n" + t + "\n" + "Now you have " + taskList.size() + " tasks in the list!");
@@ -56,17 +60,43 @@ public class Aurora {
         addToList(d);
     }
 
-    public static void addEvent(String[] argsList) {
+    public static void addEvent(String[] argsList) throws AuroraException {
+        // If no arguments provided
         if (argsList.length < 2) {
-            return;
+            throw new AuroraException("Missing argument: \"Description\".\nUsage: \"event Description /from From /to To\" ");
         }
 
         String info = argsList[1];
+        String beforeFrom = info.split("/from")[0].trim();
+        String beforeTo = info.split("/to")[0].trim();
+
         int fromDateStart = info.indexOf("/from");
         int toDateStart = info.indexOf("/to");
 
-        if (toDateStart == -1 || fromDateStart == -1 || fromDateStart > toDateStart) {
-            return;
+        // If there is no description provided
+        if (info.trim().isEmpty() || beforeFrom.isEmpty() || beforeTo.isEmpty()) {
+            throw new AuroraException("Missing argument: \"Description\".\nUsage: \"event Description /from From /to To\" ");
+        }
+
+        // If there is no /from
+        if (fromDateStart == -1) {
+            throw new AuroraException("Missing argument: \"/from From\".\nUsage: \"event Description /from From /to To\" ");
+
+        // If there is no /to
+        } else if (toDateStart == -1) {
+            throw new AuroraException("Missing argument: \"/to To\".\nUsage: \"event Description /from From /to To\" ");
+
+        // If format is in wrong order
+        } else if (fromDateStart > toDateStart) {
+            throw new AuroraException("Invalid format: \"/from From\" must be before \"/to To\".\nUsage: \"event Description /from From /to To\" ");
+
+        // If there is no details after /from
+        } else if (fromDateStart + 5 == beforeTo.length()) {
+            throw new AuroraException("Missing argument: \"From\" in \"/from From\".\nUsage: \"event Description /from From /to To\" ");
+
+        // If there is no details after /to
+        } else if (toDateStart + 3 == info.length()) {
+            throw new AuroraException("Missing argument: \"To\" in \"/to To\".\nUsage: \"event Description /from From /to To\" ");
         }
 
         Event e = new Event(info.substring(0, fromDateStart).trim(),
@@ -119,31 +149,34 @@ public class Aurora {
         while (!end) {
             String command = sc.nextLine().trim();
             String[] argsList = command.split(" ", 2);
-
-            switch (argsList[0]) {
-                case "bye":
-                    end = true; // Break out of loop
-                    break;
-                case "list":
-                    displayList();
-                    break;
-                case "mark":
-                    markTaskDone(Integer.parseInt(argsList[1]));
-                    break;
-                case "unmark":
-                    unmarkTaskDone(Integer.parseInt(argsList[1]));
-                    break;
-                case "todo":
-                    addToDo(argsList);
-                    break;
-                case "deadline":
-                    addDeadline(argsList);
-                    break;
-                case "event":
-                    addEvent(argsList);
-                    break;
-                default:
-                    addTask(argsList);
+            try {
+                switch (argsList[0]) {
+                    case "bye":
+                        end = true; // Break out of loop
+                        break;
+                    case "list":
+                        displayList();
+                        break;
+                    case "mark":
+                        markTaskDone(Integer.parseInt(argsList[1]));
+                        break;
+                    case "unmark":
+                        unmarkTaskDone(Integer.parseInt(argsList[1]));
+                        break;
+                    case "todo":
+                        addToDo(argsList);
+                        break;
+                    case "deadline":
+                        addDeadline(argsList);
+                        break;
+                    case "event":
+                        addEvent(argsList);
+                        break;
+                    default:
+                        addTask(argsList);
+                }
+            } catch (AuroraException e) {
+                printMsg(e.getMessage());
             }
         }
 
---
+++ src/main/java/AuroraException.java
@@ -0,0 +1,5 @@
+public class AuroraException extends Exception {
+    public AuroraException(String message) {
+        super(message);
+    }
+}
