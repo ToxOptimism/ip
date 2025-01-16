8ac061ef6e9f584b1e937529762422eb3dce45f2
ToxOptimism <toxicoptimismben@gmail.com>
2025-01-16 17:47:02 +0800

    Added bye command & echo user input.

--- src/main/java/Aurora.java
+++ src/main/java/Aurora.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
+
 public class Aurora {
+
+    private static final String GREETING = "Hello! I'm Aurora.\nWhat can I do for you?";
+    private static final String GOODBYE = "Bye. Hope to see you again soon!";
+
+    public static void printMsg(String msg) {
+        System.out.println("=======================");
+        System.out.println(msg);
+        System.out.println("=======================");
+    }
+
     public static void main(String[] args) {
-        System.out.println("=======================");
-        System.out.println("Hello! I'm Aurora.\nWhat can I do for you?");
 
-        System.out.println("=======================");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+        boolean end = false; // Boolean to determine if chatbot should end
 
-        System.out.println("=======================");
+        // Greet User
+        printMsg(GREETING);
+
+        // Chatbot
+        while (!end) {
+            String[] command = sc.nextLine().split(" ");
+
+            switch (command[0]) {
+                case "bye":
+                    end = true; // Break out of loop
+                    break;
+                default:
+                    printMsg(command[0]);
+            }
+        }
+
+        // Say goodbye to user
+        printMsg(GOODBYE);
     }
 }
