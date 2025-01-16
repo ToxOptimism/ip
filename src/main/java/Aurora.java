b93cc566efc4196c1d43db45ca15f68bde8606a3
ToxOptimism <toxicoptimismben@gmail.com>
2025-01-16 18:16:51 +0800

    Added add tasks to tasklist & display tasklist, Removed echo

--- src/main/java/Aurora.java
+++ src/main/java/Aurora.java
@@ -1,13 +1,30 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Aurora {
 
     private static final String GREETING = "Hello! I'm Aurora.\nWhat can I do for you?";
     private static final String GOODBYE = "Bye. Hope to see you again soon!";
+    private static ArrayList<String> taskList = new ArrayList<>();
 
     public static void printMsg(String msg) {
         System.out.println("=======================");
         System.out.println(msg);
+        System.out.println("=======================");
+    }
+
+    public static void addList(String task) {
+        taskList.add(task);
+        printMsg("added: " + task);
+    }
+
+    public static void displayList() {
+        int index = 1;
+
+        System.out.println("=======================");
+        for (String task : taskList) {
+            System.out.println(index + ". " + task);
+        }
         System.out.println("=======================");
     }
 
@@ -21,14 +38,18 @@ public class Aurora {
 
         // Chatbot
         while (!end) {
-            String[] command = sc.nextLine().split(" ");
+            String command = sc.nextLine();
+            String[] argsList = command.split(" ");
 
-            switch (command[0]) {
+            switch (argsList[0]) {
                 case "bye":
                     end = true; // Break out of loop
                     break;
+                case "list":
+                    displayList();
+                    break;
                 default:
-                    printMsg(command[0]);
+                    addList(command);
             }
         }
 
