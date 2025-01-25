8bf617d9517a94e6b2271e6c1855d523caf52e8a
ToxOptimism <toxicoptimismben@gmail.com>
2025-01-25 23:49:42 +0800

    Add JUnit parser test
    Text UI test acts as system test, making identifying individual bugs
    difficult.
    
    Automated unit tests can help to identify issues in the code easily.
    
    Let's
    * Create a ParserTest class to handle unit test

--- src/main/java/aurora/task/ToDo.java
+++ src/main/java/aurora/task/ToDo.java
@@ -15,4 +15,5 @@ public class ToDo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
 }
---
+++ src/test/java/aurora/util/ParserTest.java
@@ -0,0 +1,131 @@
+package aurora.util;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import aurora.exception.AuroraException;
+import aurora.command.Command;
+import aurora.command.AddDeadlineCommand;
+import aurora.command.AddEventCommand;
+import aurora.command.AddToDoCommand;
+import aurora.command.ByeCommand;
+import aurora.command.DeleteCommand;
+import aurora.command.ListCommand;
+import aurora.command.MarkCommand;
+import aurora.command.UnmarkCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+public class ParserTest {
+
+    private Parser parser;
+
+    @BeforeEach
+    public void setUp() {
+        parser = new Parser();
+    }
+
+    @Test
+    public void parseCommand_createListCommand() throws AuroraException {
+        Command command = parser.parseCommand("list");
+        assertInstanceOf(ListCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_markCommand() throws AuroraException {
+        Command command = parser.parseCommand("mark 1");
+        assertInstanceOf(MarkCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_unmarkCommand() throws AuroraException {
+        Command command = parser.parseCommand("unmark 1");
+        assertInstanceOf(UnmarkCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_createToDoCommand() throws AuroraException {
+        Command command = parser.parseCommand("todo borrow book");
+        assertInstanceOf(AddToDoCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_createDeadlineCommand() throws AuroraException {
+        Command command = parser.parseCommand("deadline return book /by 2/12/2019 1800");
+        assertInstanceOf(AddDeadlineCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_createEventCommand() throws AuroraException {
+        Command command = parser.parseCommand("event project meeting /from 2/12/2019 1800 /to 4/12/2019 0800");
+        assertInstanceOf(AddEventCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_createDeleteCommand() throws AuroraException {
+        Command command = parser.parseCommand("delete 8");
+        assertInstanceOf(DeleteCommand.class, command);
+    }
+
+    @Test
+    public void parseCommand_createUnknownCommand() {
+        AuroraException exception = assertThrows(AuroraException.class, () -> {
+            parser.parseCommand("testing unknown command");
+        });
+
+        assertEquals("Unknown command: testing unknown command", exception.getMessage());
+    }
+
+    @Test
+    public void parseCommand_createByeCommand() throws AuroraException {
+        Command command = parser.parseCommand("bye");
+        assertInstanceOf(ByeCommand.class, command);
+    }
+
+    @Test
+    public void parseDateTime_dateTimeValid() {
+        LocalDateTime expected = LocalDateTime.of(2019, 12, 2, 18, 0);
+        assertEquals(expected, parser.parseDateTime("2/12/2019 1800"));
+    }
+
+    @Test
+    public void parseDateTime_notADateTime() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime("notADate"));
+    }
+
+    @Test
+    public void parseDateTime_invalidDivider() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime("02-12-2019 1800"));
+    }
+
+    @Test
+    public void parseDateTime_missingTime() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime("02/12/2019"));
+    }
+
+    @Test
+    public void parseDateTime_missingDate() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime("1800"));
+    }
+
+    @Test
+    public void parseDateTime_emptyString() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime(""));
+    }
+
+    @Test
+    public void parseDateTime_nullValue() {
+        // Somewhat trivial
+        assertNull(parser.parseDateTime(null));
+    }
+}
+
