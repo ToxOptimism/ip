b72714a9e8968d063a3121a4b350b81218a76f91
ToxOptimism <toxicoptimismben@gmail.com>
2025-02-03 16:20:32 +0800

    Replace TUI with GUI
    Text based UI can make the application feel unapproachable to new users
    as they are unsure of what to do.
    
    A familiar chatbot messaging style GUI would benefit user experience.
    
    Let's
    * Replace TUI with GUI based on Duke's JavaFX tutorial.
    * Get input using the GUI.
    * Display command responses using the GUI.
    * Add a launcher class to launch the main application.
    * Update the aurora application to load the UI scene.
    * Update the aurora application to execute input on event.

--- src/main/java/aurora/command/UnmarkCommand.java
+++ src/main/java/aurora/command/UnmarkCommand.java
@@ -28,7 +28,7 @@ public class UnmarkCommand extends Command {
         super.execute(taskList, storage);
         Task t = taskList.unmarkTaskDone(index); // throws AuroraException if index is out of bounds
 
-        Ui.printMsg("This task has been marked as not done:" + "\n" + t);
+        Ui.getSingleton().printMsg("This task has been marked as not done:" + "\n" + t);
         overwriteTaskListFile(taskList, storage);
     }
 
